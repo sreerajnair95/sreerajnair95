5e981e6b1d49d0b2435ca1234033d9da772f6936
sreerajnair95 <87660195+sreerajnair95@users.noreply.github.com>
2023-12-11 12:08:46 -0600

    Create sg2.tf

---
+++ sg2.tf
@@ -0,0 +1,25 @@
+resource "aws_security_group" "allow_tls2_0" {
+  name        = "allow_tls2_0"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "vpc-04c01beec08b0c80d"
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["172.16.0.0/16"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
